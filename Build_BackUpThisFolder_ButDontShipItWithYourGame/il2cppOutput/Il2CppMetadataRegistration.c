b20e8cb2bebb14691018f1cda4ecd93f82f49ba9
yogeandblub <chrisdan926@gmail.com>
2025-12-01 16:32:49 -0500

    Personal prefabs, scenes, and Meta XR rigs

---
+++ Build_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Il2CppMetadataRegistration.c
@@ -0,0 +1,36 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+extern const int32_t* g_FieldOffsetTable[];
+extern const Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	18316,
+	g_Il2CppGenericTypes,
+	13243,
+	g_Il2CppGenericInstTable,
+	173227,
+	g_Il2CppGenericMethodFunctions,
+	57357,
+	g_Il2CppTypeTable,
+	200307,
+	g_Il2CppMethodSpecTable,
+	17163,
+	g_FieldOffsetTable,
+	17163,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
